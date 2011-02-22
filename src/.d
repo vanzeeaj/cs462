7349521b3158961e7f7e64dc9c96ffa9136f8b24
VANZEEAJ <vanzeeaj@clark.cs.uwec.edu>
2011-02-22 13:00:32 -0600

    deleted a few old files

--- src/.d
+++
@@ -1,6 +0,0 @@
-PracticalSocket.o: PracticalSocket.cc PracticalSocket.h
-blowfish.o: blowfish.cc blowfish.h
-ByteReorderingFunctions.o: ByteReorderingFunctions.cc \
-  ByteReorderingFunctions.h
-main.o: main.cc kdc.h PracticalSocket.h ByteReorderingFunctions.h \
-  server.h client.h blowfish.h common.h
