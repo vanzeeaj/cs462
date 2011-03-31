c2c6bf63607db1850e07fe591db08868888137ff
Ethan Frei <ethan.frei@gmail.com>
2011-03-30 19:52:04 -0500

    working on switching branches

--- src/linftp.d
+++ src/linftp.d
@@ -1,23 +1,23 @@
 blowfish.o: blowfish.cc blowfish.h
 client.o: client.cc client.h EncryptedSockets.h PracticalSocket.h \
-  common.h blowfish.h
+ common.h blowfish.h
 EncryptedSockets.o: EncryptedSockets.cc EncryptedSockets.h \
-  PracticalSocket.h common.h blowfish.h
+ PracticalSocket.h common.h blowfish.h
 kdc.o: kdc.cc kdc.h EncryptedSockets.h PracticalSocket.h common.h \
-  blowfish.h
+ blowfish.h
 main.o: main.cc common.h server.h EncryptedSockets.h PracticalSocket.h \
-  blowfish.h kdc.h client.h
+ blowfish.h kdc.h client.h
 PracticalSocket.o: PracticalSocket.cc PracticalSocket.h common.h
 server.o: server.cc server.h common.h EncryptedSockets.h \
-  PracticalSocket.h blowfish.h
+ PracticalSocket.h blowfish.h
 blowfish.o: blowfish.cc blowfish.h
 blowfish.o: blowfish.h
 client.o: client.h EncryptedSockets.h PracticalSocket.h common.h \
-  blowfish.h
+ blowfish.h
 common.o: common.h
 EncryptedSockets.o: EncryptedSockets.h PracticalSocket.h common.h \
-  blowfish.h
+ blowfish.h
 kdc.o: kdc.h EncryptedSockets.h PracticalSocket.h common.h blowfish.h
 PracticalSocket.o: PracticalSocket.h common.h
 server.o: server.h common.h EncryptedSockets.h PracticalSocket.h \
-  blowfish.h
+ blowfish.h
